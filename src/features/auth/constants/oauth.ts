cfcda23c7660383c399a2c7175442e3629bcd0c3
Timoideas <Timoideas@hotmail.com>
2025-04-30 09:07:13 -0500

    ppa

--- src/features/auth/constants/oauth.ts
+++ src/features/auth/constants/oauth.ts
@@ -5,10 +5,11 @@ export const TIKTOK_OAUTH_TOKEN_URL =
   "https://open.tiktokapis.com/v2/oauth/token/";
 export const TIKTOK_OAUTH_FIELDS =
   "fields=open_id,avatar_url,username,display_name,bio_description";
-export const TIKTOK_INTERNAL_REDIRECT_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/auth/tiktok/callback"
-    : "https://create-timoideas-frontend.vercel.app/auth/tiktok";
+export const TIKTOK_INTERNAL_REDIRECT_URL = "http://localhost:3000/auth/tiktok";
+// export const TIKTOK_INTERNAL_REDIRECT_URL =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:3000/auth/tiktok/callback"
+//     : "https://create-timoideas-frontend.vercel.app/auth/tiktok";
 // export const TIKTOK_EXTERNAL_REDIRECT_URL =
 //   process.env.NODE_ENV === "development"
 //     ? "https://create-timoideas-frontend.vercel.app/auth/callback"
